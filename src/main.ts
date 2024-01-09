ac1dced106b19eaeb6507931ec5660fd3917049f
Bimendra Bandara <bimendra.bandara@velaris.io>
2024-01-10 00:09:33 +0800

    Initialize project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Prisma - NestJS Demo')
+    .setDescription('A demo API for Prisma & NestJS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Prisma NestJS',
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
